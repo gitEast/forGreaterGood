2452d7e4cc20d81f67c78bb02bec4f6084908fc9
East <linxiayoudongfeng@gmail.com>
2021-11-12 17:44:16 +0800

    2021-11-12 Vue3 中的动画 + Mixin + extends + ref + reactive + toRef + toRefs

---
+++ vue3/15-code/02_composition_api/src/App.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-12 14:13:52
+ * @LastEditTime: 2021-11-12 15:31:23
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \forGreaterGood\vue3\15-code\02_composition_api\src\App.vue
+-->
+<template>
+  <div>
+    <home-page :msg="msg"></home-page>
+    <div>
+      <!-- <h2>counter: {{ state.counter }}</h2> -->
+      <h2>counter: {{ counter }}</h2>
+      <button @click="increment">add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue'
+import HomePage from "./pages/HomePage.vue"
+export default {
+  setup(props) {
+    // const state = reactive({counter: 100})
+    const counter = ref(100)
+    const increment = () => {
+      // state.counter++
+      counter.value++
+    }
+    const msg = 'hello'
+    return {
+      // state,
+      counter,
+      increment,
+      msg
+    }
+  },
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue3/15-code/02_composition_api/src/pages/HomePage.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-12 14:26:02
+ * @LastEditTime: 2021-11-12 14:51:32
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \forGreaterGood\vue3\15-code\02_composition_api\src\pages\HomePage.vue
+-->
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  setup(props) {
+    console.log(props)
+    return {
+      msg: props.msg
+    }
+  }
+}
+</script>
+<style scoped>
+  
+</style>
